c1b3a4226889326ca5719fbc9f47c9b44d705ea1
abrowntx <ascaps@gmail.com>
2018-10-24 00:27:33 -0500

    tyio

--- index.php
+++ index.php
@@ -1,14 +1,10 @@
 <?php
-//Call DBHandler
-    //require_once('inc/inc.db.php');
 //Call SMALL LOGO template file
-require_once('template.smalllogo.php');
+    require_once('template.smalllogo.php');
 //Call HEADER template file
     require_once('template.header.php');
-    
 //Call CTE TRAINING template file
     require_once('template.CTE.php');
-
 //Call FOOTER template file
     require_once('template.footer.php');
 ?>
